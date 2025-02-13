a341a6044b1202e3a350c191647cd03a1129f414
sachin <125rathodsachin@gmail.com>
2025-02-13 13:35:55 +0530

    assets: logo and animation added

---
+++ lib/core/constants/asset_constants.dart
@@ -0,0 +1,7 @@
+class AssetConstants {
+  // App Icons
+  static const String appLogo = "assets/images/icons/appLogo.svg";
+
+  // Animation
+  static const String logoAnimation = "assets/animations/logo.json";
+}
